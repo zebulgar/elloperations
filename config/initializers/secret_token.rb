ac661f67cfb7daba10c64ab913db1bbac3da10ff
Delian Asparouhov <me@delian.io>
2013-04-05 13:24:47 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elloperations::Application.config.secret_token = 'cab311acb09f80fe7587d3e5cc2ba7a4969cccd9264e4489ac02b5d22e08278c62bae05c24ddfeac77136f385bf04a1471ca2515138a600224dcedecce409a1d'
